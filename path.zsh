0e7a4459fe214b98e217a3faf245d2102e537ea0
Gerard Nguyen <gerard@Gerards-MacBook-Pro.local>
2024-01-13 08:22:13 +1100

    update path

--- path.zsh
+++ path.zsh
@@ -4,7 +4,4 @@ export PATH="$PATH:$GOPATH/bin"
 export PATH="$PATH:/usr/local/sbin"
 export PATH="$PATH:/usr/local/bin"
 export PATH=$HOME/usr/local/bin:$PATH
-export PATH=$HOME/Library/Android/sdk/platform-tools:$PATH
 export PATH="$PATH:/Users/gerard/Library/Application Support/multipass/bin"
-export PATH="$PATH:$(dirname $(func-e which))"
-export PATH="$PATH:/Users/gerard/dev/istio/bin/dev"
